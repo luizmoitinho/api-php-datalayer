5730478dbefb460a1a54bab8b52901a1025caff6
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-05-06 21:02:16 -0300

    Post user in database

--- source/Models/User_Model.php
+++ source/Models/User_Model.php
@@ -4,10 +4,11 @@ namespace Source\Models;
 
 use CoffeeCode\DataLayer\DataLayer;
 
-class User extends DataLayer{
+class User_Model extends DataLayer{
+
 
     public function __construct(){
         //string "TABLE_NAME", array ["REQUIRED_FIELD_1", "REQUIRED_FIELD_2"], string "PRIMARY_KEY", bool "TIMESTAMPS"
-        parent::__construct('tb_usuario',['nome','email','usuario','senha'],'id',false);
+        parent::__construct('tb_usuario',array('nome_usuario','email_usuario','usuario','senha'),'id_usuario',false);
     }
 }
--- source/Models/Validations.php
+++ source/Models/Validations.php
@@ -5,7 +5,7 @@ namespace Source\Models;
 final class Validations{
 
     public static function validationString(string $string){
-        return strlen($string) >= 3 && is_numeric($string);
+        return strlen($string) >= 3 && !is_numeric($string);
     }
 
     public static function validateEmail(String $email){
@@ -16,5 +16,34 @@ final class Validations{
         return filter_var($integer,FILTER_VALIDATE_INT);
     }
 
+
+    public static function validateFormCad($data){
+        $errors =  array();
+        if(!Validations::validationString($data->nome_usuario)){
+            array_push($errors,"Nome");
+        }
+        if(!Validations::validateEmail($data->email_usuario)){
+            array_push($errors,"E-mail");
+        }
+        if(!Validations::validationString($data->usuario)){
+            array_push($errors,"Usuário");
+        }
+        if(!Validations::validationString($data->senha)){
+            array_push($errors,"Senha");
+        }
+
+        return count($errors) > 0 ? $errors:TRUE;
+    }
+
+    
+    public static function setHeaderResponse(string $header,string $response,$fields = null){
+        header("http/1.1".$header);
+        if($fields)
+            echo json_encode( array('reponse'=>$response,'fields'=>$fields));
+        else
+            echo json_encode( array('reponse'=>$response));
+
+
+    }
 
 }
