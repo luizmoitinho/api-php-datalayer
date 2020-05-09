235bf96035180aa84d1b16449c8830de389b97c8
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-05-09 14:29:11 -0300

    Fixed script sql

--- script.sql
+++ script.sql
@@ -1,7 +1,15 @@
 
-drop database if exists tb_usuario;
 
-create database tb_usuario;
+create database db_api_rest;
 use db_api_rest;
+
+create table tb_usuario(
+    id_usuario int not null primary key,
+    nome_usuario varchar(45) not null,
+    email_usuario varchar(30) not null,
+    usuario varchar(15) not null,
+    senha varchar(8) not null
+)
+
 alter table tb_usuario add constraint `pk_tb_usuario` primary key(id_usuario);
 alter table tb_usuario change id_usuario id_usuario int not null auto_increment;
